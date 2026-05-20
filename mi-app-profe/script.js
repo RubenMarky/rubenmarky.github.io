aac74a5a8733b15bd2bd9089bdd89ee280dc8bd6
RubenMarky <rubenmarky@yahoo.com.ar>
2026-05-19 23:27:39 -0300

    Update script.js

--- mi-app-profe/script.js
+++ mi-app-profe/script.js
@@ -1,13 +1,16 @@
-let alumnos = [];
+let alumnos = JSON.parse(localStorage.getItem("alumnos")) || [];
+
+mostrarAlumnos();
 
 function agregarAlumno() {
   let nombre = document.getElementById("nombre").value;
   let nota = document.getElementById("nota").value;
 
-  if(nombre === "" || nota === "") return;
+  if (nombre === "" || nota === "") return;
 
   alumnos.push({ nombre, nota });
 
+  guardarDatos();
   mostrarAlumnos();
 }
 
@@ -15,9 +18,26 @@ function mostrarAlumnos() {
   let lista = document.getElementById("lista");
   lista.innerHTML = "";
 
-  alumnos.forEach(alumno => {
+  alumnos.forEach((alumno, index) => {
     let li = document.createElement("li");
     li.textContent = alumno.nombre + " - Nota: " + alumno.nota;
+
+    // Botón eliminar 👇
+    let btn = document.createElement("button");
+    btn.textContent = "❌";
+    btn.onclick = () => eliminarAlumno(index);
+
+    li.appendChild(btn);
     lista.appendChild(li);
   });
+}
+
+function guardarDatos() {
+  localStorage.setItem("alumnos", JSON.stringify(alumnos));
+}
+
+function eliminarAlumno(index) {
+  alumnos.splice(index, 1);
+  guardarDatos();
+  mostrarAlumnos();
 }
